81930fc1f6f59399568ea491372525d65a1a192f
CoreHoundSystem <51447286+CoreHoundSystem@users.noreply.github.com>
2019-06-09 23:49:50 -0400

    Create Channelscript.js

---
+++ Channelscript.js
@@ -0,0 +1,143 @@
+function like(x) {
+		if($(x).hasClass('liked')) {
+			newLikes=[];
+			oldLikes=thisChannel.liked.split("|");
+			for(var i=0;i<oldLikes.length;i++) {
+				if(oldLikes[i]!=currentSong.id) {
+					newLikes.push(oldLikes[i]);
+				}
+			}
+			thisChannel.liked=newLikes.join("|");
+		} else {
+			thisChannel.liked=thisChannel.liked + "|" + currentSong.id;
+		}
+		$(x).toggleClass('liked');
+		updateData(channelForm,thisChannel);		//??	formURL,object
+	}
+
+	function dislike(x) {
+		if($(x).hasClass('disliked')) {
+			
+		} else {
+			thisChannel.liked=thisChannel.liked + "|" + currentSong.id;
+			updateData(channelForm,thisChannel);	//??
+			playSong();
+		}
+	}
+
+	//fix this function?
+	function togglePause() {
+		audio=document.getElementById("activeSong");
+		if(audio.paused) {
+			audio.play();
+		} else {
+			audio.pause();
+		}
+	}
+
+	function trackTime(x) {
+		$('#trackTime').css('background-image','linear-gradient(to right, var(--tColor) ' + ((x.currentTime / x.duration) * 100) + '%, var(--bColor) 0%)');
+	}
+
+	function buildActiveSong(x) {
+		console.log(x);
+		$('#activeChannel').empty();
+		$('#activeChannel').append('<audio id="activeSong" src="' + songURL + x.link + '" autoplay="true" type="audio/mp3" volume="1.0" ontimeupdate="trackTime(this)"></audio>');
+		
+		$('#activeChannel').append('<div class="backOrMin"></div>')
+		//back/minimize menu  -- toggles
+		//channel name -- hides on min
+		//image -- hides on min
+		//song title -- prominent on min
+		//artist --  less prominent on min
+		//albumevent -- hide on min
+		
+		
+		$('#activeChannel').append('<div id="trackControls"><div class="upperRow"></div><div class="lowerRow"></div></div>');
+		$('.upperRow').append('<div><span><span><div></div><span></span></div>');
+			//hide on min
+		$('.lowerRow').append('');
+			//pause/play
+			//nexttrack
+			//like/dislikes
+				//hide on min
+			
+		$('.backOrMin').click(function() {
+			$(this).parent().toggleClass('minimize');
+		});
+		
+		
+		
+		audio=document.getElementById("activeSong");
+		audio.play();
+		$('#activeSong').on('ended',function() {
+			playSong();
+		});
+	}
+
+	function playSong() {
+		songNumber=Math.round(Math.random()*cSongs.length);
+		if(rPlayed.indexOf(cSongs[songNumber])!=-1||(songNumber<0||songNumber>=cSongs.length)) {
+			playSong();
+		} else {
+			commercialTicker++;
+			window["currentSong"]=cSongs[songNumber];
+			songMedia="";
+			if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+				if ('mediaSession' in navigator) {
+					navigator.mediaSession.metadata = new MediaMetadata({
+						title: currentSong.title,
+						artist: currentSong.artist,
+						album: currentSong.albumEvent,
+						artwork: [
+							{src:songMedia,sizes:'96x96',type:'image/png'},
+							{src:songMedia,sizes:'128x128',type:'image/png'},
+							{src:songMedia,sizes:'192x192',type:'image/png'},
+							{src:songMedia,sizes:'256x256',type:'image/png'},
+							{src:songMedia,sizes:'384x384',type:'image/png'},
+							{src:songMedia,sizes:'512x512',type:'image/png'},
+						]
+					});
+					navigator.mediaSession.setActionHandler('play',function() {togglePause()});
+					navigator.mediaSession.setActionHandler('pause',function() {togglePause()});
+					//navigator.mediaSession.setActionHandler('seekbackward', function() {});
+					//navigator.mediaSession.setActionHandler('seekforward', function() {});
+					navigator.mediaSession.setActionHandler('nexttrack',function() {playSong()});
+				}
+			}
+			buildActiveSong(currentSong);
+		}
+	}
+
+	function startChannel(c) {
+		artist=c.artist;
+		likes=[];
+		likes=c.likes.split("|");
+		dislikes=c.dislikes.split("|");
+		genres=[];
+		window["cSongs"]=[];
+		window["rPlayed"]=[];
+		window["commercialTicker"]=0;
+		for(var i=0;i<likes.length;i++) {
+			sIndex=0;
+			for(var j=0;j<songs.length;j++) {
+				if(songs[j].songID==likes[i]) {
+					sIndex==j;
+				}
+			}
+			if(genres.indexOf(songs[sIndex].genre)==-1) {
+				genres.push(songs[sIndex].genre);
+			}
+		}
+		console.log(genres);
+		for(var i=0;i<songs.length;i++) {
+			console.log(songs[i]);
+			console.log(dislikes.indexOf(songs[i].songID));
+			console.log(songs[i].genre);
+			if((songs[i].artist==artist||genres.indexOf(songs[i].genre)!=-1)&&dislikes.indexOf(songs[i].songID)==-1) {
+				cSongs.push(songs[i]);
+				console.log("Sorted " + songs[i]);
+			}
+		}
+		playSong();
+	}
