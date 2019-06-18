fd9402b367359ac273b3b2a82a285e80b8954633
CoreHoundSystem <51447286+CoreHoundSystem@users.noreply.github.com>
2019-06-18 18:08:21 -0400

    Update Users.js

--- Users.js
+++ Users.js
@@ -1,165 +1,255 @@
-registerURL="https://docs.google.com/forms/d/e/1FAIpQLSes-beRIp1-A2SgZP9CS-8F1dr0E71Z63RlwjaS1nnWuafQ5w/formResponse?usp=pp_url&entry.762123824="	//register gID
-syncURL="https://docs.google.com/forms/d/e/1FAIpQLSe-bJWth2cKQ7-jo0vOHD_mSaIQIQfJN4aFcskKBfMaeH7zhw/formResponse?usp=pp_url&entry.762123824=";	//sync gID to UUID
-artist="https://docs.google.com/forms/d/e/1FAIpQLSdkyoQyqkHLjMPNbYa18RwcTLdP4ViqJXUt259MM4xUs_BCaw/formResponse?usp=pp_url&entry.762123824=";	//Adding artist uuid
-channels="https://docs.google.com/forms/d/e/1FAIpQLSdlQGMcv_XxMOywJUWwFVVDKOG2elnR247wVMsggyFjUgZhZQ/formResponse?usp=pp_url&entry.762123824=";	//Channel list
-songs="https://docs.google.com/forms/d/e/1FAIpQLSf2TJ7YVxt1cqVSBoQIJysDuBFpJ-WMTgnNUvj5lU7xj-PhZQ/formResponse?usp=pp_url&entry.762123824=";	//Song list
-
-userRegistry="1pg-HrcwZZc26c5d9DIyG1AKqCUG-rwEQAc_Zh7aVXZ8";	//TRUE get info from registry
-userSyncs="1GDyPff8QkQynVWfOWZIFNli4Lz8NtScWPc1U5uIwOOM";		//TRUE get syncd uuid for gID
-syncdProfile="1wXxggUlx2td4EatduNW47cgxVX91xN5k7EYcFtjzoak";
-//syncdProfile="1zylDFVqtUmkMFULDmxzhLyi6JZr0lRhE4Kir4dt0yrk"	//TRUE get artists profiles
-//channelKey="1i9SfwLkDj40yOqOmRMAhe4FXI5fjjjyufx8Nz_IPKbg";	//temp
-channelKey="1uBEt-sN0Ah00fu2Y9q8BfFMOzWyetaTLfZ0AAyp9Mro";	//TRUE get channels
-//songKey="1EHmHCY8-n9wVSkimbL2k7d7NMZDo8xSsrtIuaRRJwNM";
-songKey="1TyvPyzsS43sGyANpbeOjwP3HSQFpH9JtcxXYbe8Aj6w";		//TRUE get songs
-
-function dataPulls(x) {
-	dP[x]=1;
-	dPSum=0;
-	for(var i=0;i<Object.keys(dP).length;i++) {
-		dPSum+=dP[Object.keys(dP)[i]];
-	}
-	if(dPSum==Object.keys(dP).length) {
-		console.log(x);
-		pSongs=[];
-		listChannels();
-		if(profile.uuid.length==36) {
-			for(var i=0;i<songs.length;i++) {
-				if(songs[i].artist==profile.uuid) {
-					pSongs.push(songs[i].songID);
-				}
-			}
-			profile.songs=pSongs;
-		}
-		cL(profile);
-	}
-}
-
-function getSongs(s,x,y) {
-	checkSongs=[];
-	window[s]=[];
-	$(function() {
-		$.getJSON("https://spreadsheets.google.com/feeds/list/" + x + "/" + y + "/public/values?alt=json-in-script&callback=?",
-		function (data) {
-			$.each(data.feed.entry, function(i,entry) {
-				thisEntry=JSON.parse(entry.gsx$data.$t.replace(/'/g,"\""));
-				if(checkSongs.indexOf(thisEntry.songID)==-1) {
-					checkSongs.push(thisEntry.songID);
-					window[s].push(thisEntry);
-				} else {
-					window[s][checkSongs.indexOf(thisEntry.songID)]=thisEntry;
-				}
-			});
-			dataPulls("getSongs");
-		});
-	});
-}
-
-function getChannels(g,c,x,y) {
-	checkChannels=[];
-	window[c]=[];
-	$(function() {
-		$.getJSON("https://spreadsheets.google.com/feeds/list/" + x + "/" + y + "/public/values?alt=json-in-script&callback=?",
-		function (data) {
-			$.each(data.feed.entry, function(i,entry) {
-				if((entry.gsx$data.$t.indexOf("'owner':'" + g)!=-1||entry.gsx$data.$t.indexOf('"owner":"' + g)!=-1)&&entry.gsx$data.$t.indexOf("chanID")!=-1) {
-					thisEntry=JSON.parse(entry.gsx$data.$t.replace(/'/g,"\""));
-					if(thisEntry.chanID.length>4) {
-						if(checkChannels.indexOf(thisEntry.chanID)==-1) {
-							checkChannels.push(thisEntry.chanID);
-							window[c].push(thisEntry);
-						} else {
-							window[c][checkChannels.indexOf(thisEntry.chanID)]=thisEntry;
-						}
-					}
-				}
-			});
-			profile.channels=window[c];
-			dataPulls("getChannels");
-		});
-	});
-}
-
-function getProfile(a,x,y) {
-	window[a]=[];
-	$(function() {
-		$.getJSON("https://spreadsheets.google.com/feeds/list/" + x + "/" + y + "/public/values?alt=json-in-script&callback=?",
-		function (data) {
-			$.each(data.feed.entry, function(i,entry) {
-				thisImage=entry.gsx$image.$t;
-				//thisImage=thisImage.replace("https://secondlife.com/app/image/","");
-				//thisImage=thisImage.replace("/1","");
-				//thisImage="https://texture-service.agni.lindenlab.com/" + thisImage + "/256x192.jpg/";
-				thisEntry=JSON.parse('{"uuid":"' + entry.gsx$data.$t + '","name":"' + entry.gsx$name.$t + '","image":"' + thisImage + '"}');
-				window[a].push(thisEntry);
-			});
-			dataPulls("getProfile");
-			buildArtistSearch();
-		});
-	});
-}
-
-function checkSync(g,x,y) {
-	var syncChecker=0;
-	window["sync"]="NA";
-	$(function() {
-		$.getJSON("https://spreadsheets.google.com/feeds/list/" + x + "/" + y + "/public/values?alt=json-in-script&callback=?",
-		function (data) {
-			$.each(data.feed.entry, function(i,entry) {
-				if(entry.gsx$data.$t.indexOf(g)===0) {
-					window["sync"]=entry.gsx$data.$t.split("|");
-					profile.uuid=window["sync"][1];
-				}
-			});
-			if(syncChecker==0) {
-				//prompt user to sync
-			}
-			if(syncChecker>1) {
-				//error?
-			}
-			dataPulls("checkSync");
-		});
-	});
-}
-
-function checkRegistry(g,m,f,l,x,y) {
-	var regChecker=0;
-	$(function() {
-		$.getJSON("https://spreadsheets.google.com/feeds/list/" + x + "/" + y + "/public/values?alt=json-in-script&callback=?",
-		function (data) {
-			$.each(data.feed.entry, function(i,entry) {
-				if(entry.gsx$data.$t.toString()==g.toString()) {
-					regChecker++;
-				}
-			});
-			if(regChecker==0) {
-				sendEventToAnalytics("user","register",g,registerURL,g);
-				
-				//setTimeout(function() {
-					//checkRegistry(g,m,f,l,q,x,y);
-				//},2000);
-			} else {
-				//what to do if checker is > 0
-			}
-			dataPulls("checkRegistry");
-		});
-	});
-}
-
-function loadUser(p) {
-	console.log(p);
-	window["profile"]=p;
-	cL([profile.gID,profile.gMail,profile.firstName,profile.lastName].join("|"));
-	checkRegistry(p.gID,p.gMail,p.firstName,p.lastName,userRegistry,"1");
-	checkSync(p.gID,userSyncs,"1");
-	
-	getChannels(p.gID,"myChannels",channelKey,"1");	
-	//fix this...
-	
-}
+popSong="https://docs.google.com/forms/d/e/1FAIpQLScBJbUX_ddqJexIpcHXAAB1dNfytH46KCp-jazfGl8kktaIlA/formResponse?usp=pp_url&entry.762123824=";
+popGenres="https://docs.google.com/forms/d/e/1FAIpQLSf5qoAw2zz7LAs6nXOfKaZB2fMqMzi6k8Kmx4s57opwP0JW1Q/formResponse?usp=pp_url&entry.762123824=";
+songOpinion="https://docs.google.com/forms/d/e/1FAIpQLSeS_aRew-vd3OgJ-SQPHkPhh-M_oliItmEgbRvYISvaVjxs6w/formResponse?usp=pp_url&entry.762123824=";
 
 $(function() {
-	dP=JSON.parse('{"checkRegistry":0,"checkSync":0,"getProfile":0,"getChannels":0,"getSongs":0}');
-	getProfile("artists",syncdProfile,"1");
-	getSongs("songs",songKey,"1");
+	console.log("Library.js Loaded");
+	if(window.navigator.userAgent.indexOf("MSIE ") > 0 || window.navigator.userAgent.indexOf("Trident") > 0) {
+		alert("Due to growing security concerns with Internet Explorer, this site will nolonger load on this browser.");
+		console.log("IE");
+		rStyle = $('#rootStyles').text();
+		rStyle = rStyle.substring(rStyle.indexOf('{')+1,rStyle.indexOf('}'));
+		rArray = rStyle.split(';');
+		rObject = {};
+		for(var i=0;i<rArray.length-1;i++) {
+			if(rArray[i].length > 3) {
+				thisPair = [];
+				thisPair = rArray[i].split(':');
+				thisPair[0] = thisPair[0].replace(/[\s\n]/g,"");
+				thisPair[1].replace(/[\s\n]/g,"").substring(1,-2);
+				rObject[thisPair[0]] = thisPair[1];
+			}
+		}
+		$('style').each(function() {
+			if($(this).text().indexOf('var(--') != -1) {
+				theseStyles = $(this);
+				$.each(rObject, function(key,value) {
+					k = 'var(' + key + ')';
+					kReg = new RegExp(k,'g');
+					replaceAll(theseStyles,k,value);
+				});
+			}
+		})
+	} else {
+		console.log(window.navigator.userAgent);
+	}
 })
+
+function replaceAll(x,k,value) {
+	x.text(function () {
+		x.text(x.text().replace(k,value)); 
+	});
+	if(x.text().indexOf(k) != -1) {
+		replaceAll(x,k,value);
+	}
+}
+
+function secondsToTime(x) {
+	time=Math.round(Math.floor(x/60)) + ":";
+	if(Math.round(x -= Math.floor(x/60)*60)<10) {
+		time=time+"0" + Math.round(x -= Math.floor(x/60)*60);
+	} else {
+		time=time+Math.round(x -= Math.floor(x/60)*60);
+	}
+	return time;
+}
+
+function prepObjectForSubmission(x) {
+	return JSON.stringify(x).replace(/"/g,"'")
+}
+
+function getArtist(x) {
+	for(var i=0;i<artists.length;i++) {
+		if(artists[i].uuid==x) {
+			return artists[i];
+		}
+	}
+}
+
+function sendEventToAnalytics(x,y,z,f,d) {		//Sends event to Google Analytics - requires analytics
+	console.log(x,y,z);
+	if(d) {
+		console.log(f,d);
+		submitData(f,d);
+	}
+	gtag('event',y, {
+		'event_category': x,
+		'event_label': z
+	});
+}
+
+function submitData(f,d) {
+	$('body').append('<iframe style="display:none" src="' + f + d + '">');
+}
+
+function updateChannel(x) {
+	console.log(x);
+	for(var i=0;i<myChannels.length;i++) {
+		if(myChannels[i].chanID==x.chanID) {
+			console.log(myChannels[i]);
+		}
+	}
+	submitData(channels,encodeURIComponent(JSON.stringify(x)));
+}
+
+function isLiked(x) {
+	if(thisChannel.likes.length>0&&thisChannel.likes.indexOf(x.songID)!=-1) {
+		return " liked";
+	} else {
+		return "";
+	}
+}
+
+function buildArtistSearch() {
+	$('#landingModal').find('.artistSearch').focus(function() {
+		$(this).parent().next('.artistList').addClass('show');
+	})
+	$('.artistSearch').focus(function() {
+		
+	})
+	$('.artistSearch').blur(function() {
+		setTimeout(function() {
+			$('.artistSearch').parent().next().removeClass('show');
+		}, 200);
+	})
+	$('.artistSearch').on('input',function() {
+		if($(this).val().length>=1) {
+			$(this).parent().next('.artistList').addClass('show');
+		}
+		artistName=$(this).val();
+		artistArray=[];
+		$('.artistSearch').each(function() {
+			if($(this).val().length>=1&&!$(this).parents('#landingModal').length) {
+				artistArray.push($(this).val());
+			}
+		})
+		console.log(artistArray);
+		$('.artistList div.artistOption').each(function() {
+			if($(this).text().toLowerCase().indexOf(artistName.toLowerCase())==-1||artistArray.indexOf($(this).text())!=-1) {
+				$(this).addClass('hide');
+			} else {
+				if(artistArray.indexOf($(this).text())==-1) {
+					$(this).removeClass('hide');
+				}
+			}
+		});
+		if(($(this).parent().next().find('.artistOption').length-$(this).find('.artistOption.hide').length)==1) {
+			$('#createChannel').addClass('available');
+		} else {
+			$('#createChannel').removeClass('available');
+		}
+	});
+	for(var i=0;i<artists.length;i++) {
+		$('.artistList').append('<div class="artistOption" name="' + artists[i].uuid + '">' + artists[i].name + '</div>');
+	}
+	$('.artistSearch').change(function() {
+		if(!$(this).parents('#landingModal').length&&$('.artistOption').text().indexOf($(this).val())!=-1) {
+			$(this).addClass('chosen');
+			if($(this).parent().parent().next().hasClass('hide')) {
+				$(this).parent().parent().next().removeClass('hide');
+			}
+		}
+	})
+	$('.artistOption').click(function() {
+		$(this).parent().prev().children('.artistSearch').val($(this).text());
+		if($(this).parents('#landingModal').length) {
+			$('#createChannel').addClass('available');
+			startCreateChannel($(this).attr('name'));
+		} else {
+			$(this).parent().prev().children('.artistSearch').addClass('chosen');
+			if($(this).parent().parent().next().hasClass('hide')) {
+				$(this).parent().parent().next().removeClass('hide');
+			}
+		}
+	})
+	$('#createChannel').click(function() {
+		if($('#createChannel').hasClass('available')) {
+			$(this).parent().find('.artistOption').each(function() {
+				if(!$(this).hasClass('hide')) {
+					startCreateChannel($(this).attr('name'));
+				}
+			});
+		}
+	})
+	$('#joinTether').click(function() {
+		$('.abcRioButton').click();
+	})
+	$('#buildChannel').click(function() {
+		newChannel=JSON.parse('{"chanID":"' + getUnixTimeStamp("C",profile.gID,Math.floor(new Date()/1000).toString(16)) + '","owner":"' + profile.gID+ '","artist":"' + theseArtists() + '","name":"' + $('#channelName').val() + '","likes":"","dislikes":"","genres":""}');
+		profile.channels.push(newChannel);
+		listChannels()
+		submitData(channels,encodeURIComponent(JSON.stringify(newChannel)));
+	})
+}
+
+function startCreateChannel(x) {
+	if(typeof profile==='object') {
+		console.log("TRUE");
+	} else {
+		$('.abcRioButton').click();
+		console.log("FALSE");
+	}
+	media=0;
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		media=$(window).height();
+	}
+	$('html body').animate({
+        scrollTop: $('#creationBox').offset().top+media
+	},1000);
+	$('#creationBox').find('.artistSearch').first().val($('#landingModal .artistSearch').val());
+	$('#creationBox').find('.artistSearch').first().addClass('chosen');
+	$('#creationBox').find('.artistBox').first().next().removeClass('hide');
+	$('#channelName').on('input',function() {
+		firstFifteen=$(this).val().substring(0,15);
+		checker=true;
+		for(var i=0;i<firstFifteen.length;i++) {
+			if(!firstFifteen.substring(i,i+1).replace(/[a-zA-Z0-9 ]/g,"")=="") {
+				checker=false
+			}
+		}
+		//typeof firstFifteen[i] == 'number'||
+		if(checker&&$(this).val().length>0) {
+			$('#buildChannel').addClass('available');
+			$('#buildChannel').text("Build " + $(this).val());
+			
+		} else {
+			$('#buildChannel').removeClass('available');
+		}
+	})
+	
+	
+	//getUnixTimeStamp("C",profile.gID,Math.floor(new Date()/1000).toString(16));
+}
+
+function theseArtists() {
+	artistList=[];
+	$('#creationBox').find('.artistSearch').each(function() {
+		console.log($(this).val());
+		for(var i=0;i<artists.length;i++) {
+			
+			if(artists[i].name==$(this).val()) {
+				console.log(artists[i].uuid);
+				artistList.push(artists[i].uuid);
+			}
+		}
+		console.log(artistList.join("|"));
+		
+	})
+	return artistList.join("|");
+}
+
+function getUnixTimeStamp(c,i,t) {
+	return c+i+"-"+t;
+}
+
+function listChannels() {
+	for(var i=0;i<profile.channels.length;i++) {
+		if(!$('#' + profile.channels[i].chanID).length) {
+			console.log(profile.channels[i]);
+			$('#myChannels').prepend('<div id="' + profile.channels[i].chanID + '" class="channelBox" name="' + i + '"><div style="background-image:url(' + getArtist(profile.channels[i].artist.substring(0,36)).image + ')"></div><div style="background-image:url(' + getArtist(profile.channels[i].artist.substring(0,36)).image + ')"></div><span>' + profile.channels[i].name + '</span></div>');
+		}
+	}
+	$('.channelBox').click(function() {
+		startChannel(myChannels[parseInt($(this).attr('name'))]);					
+	});
+}
